682815e30e84fd01606365ce140e8aed171b7dff
James Harvey <james.harvey@yahoo.com>
2014-11-26 18:59:49 +0900

    log collector in powershell

---
+++ collector.ps1
@@ -0,0 +1,21 @@
+﻿
+$archiveDir = get-item C:\Users\James\projects\HearthStoneDebugLog\archive
+
+$logFile = get-item "E:\Program Files (x86)\Hearthstone\Hearthstone_Data\output_log.txt"
+
+$archivedFiles = Get-ChildItem $archiveDir | Sort-Object -Property CreationTime -Descending
+
+if (($archivedFiles.Count -gt 0) -and ($archivedFiles[0].CreationTime -gt $logFile.CreationTime )) {
+    # the archive is more recent than the source, so bail
+    exit
+}
+
+$hearthstoneProcessCount=Get-Process Hearthstone -ErrorAction SilentlyContinue | Measure-Object
+
+if ( $hearthstoneProcessCount.Count -lt 1 ) {
+    # no hearthstone process running, so copy away!
+    $dateTimeString=Get-Date -Format yyyyMMddHHmm
+    Push-Location $archiveDir
+    Copy-Item $logFile "$dateTimeString.txt"
+    Pop-Location
+}   
