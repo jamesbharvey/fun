68400df9b6af19eaa63ea24b602b8b85e2816357
James Harvey <james.harvey@yahoo.com>
2014-12-08 16:16:43 +0900

    don't coppy the log file over if it is size zero

--- collector.ps1
+++ collector.ps1
@@ -5,7 +5,7 @@ $logFile = get-item "E:\Program Files (x86)\Hearthstone\Hearthstone_Data\output_
 
 $archivedFiles = Get-ChildItem $archiveDir | Sort-Object -Property CreationTime -Descending
 
-if (($archivedFiles.Count -gt 0) -and ($archivedFiles[0].CreationTime -gt $logFile.CreationTime )) {
+if (($archivedFiles.Count -gt 0) -and ($archivedFiles[0].CreationTime -gt $logFile.CreationTime ) -and ($archivedFiles[0].Length -gt 0)) {
     # the archive is more recent than the source, so bail
     exit
 }
