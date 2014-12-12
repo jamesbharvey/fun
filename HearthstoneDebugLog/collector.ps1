8f3149f147bc96dcea13484eecacf86c53e88a7e
James Harvey <bumblepuppy@users.noreply.github.com>
2014-12-12 12:25:41 +0900

    proper fix to not collect zero length log files

--- HearthstoneDebugLog/collector.ps1
+++ HearthstoneDebugLog/collector.ps1
@@ -3,9 +3,15 @@ $archiveDir = get-item C:\Users\James\projects\fun\HearthstoneDebugLog\archive
 
 $logFile = get-item "E:\Program Files (x86)\Hearthstone\Hearthstone_Data\output_log.txt"
 
+
+# don't copy zero length log files
+if ($logFile.Length -eq 0) {
+    exit
+}
+
 $archivedFiles = Get-ChildItem $archiveDir | Sort-Object -Property CreationTime -Descending
 
-if (($archivedFiles.Count -gt 0) -and ($archivedFiles[0].CreationTime -gt $logFile.CreationTime ) -and ($archivedFiles[0].Length -gt 0)) {
+if (($archivedFiles.Count -gt 0) -and ($archivedFiles[0].CreationTime -gt $logFile.CreationTime )) {
     # the archive is more recent than the source, so bail
     exit
 }
