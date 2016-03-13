da137946e21022bbbcc3caab675618203f71355e
unknown <james@yarigai.URIAHDOM.local>
2016-03-13 16:19:52 +0900

    made $PATH logic check to see if things exist first

--- dotfiles/Microsoft.PowerShell_profile.ps1
+++ dotfiles/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,23 @@
-Add-PathVariable -Name Path "C:\Program Files (x86)\Git\bin"
-Add-PathVariable -Name Path "C:\Users\James\sysinternals"
-Add-PathVariable -Name Path "C:\Program Files (x86)\ErgoEmacs\bin"
-Add-PathVariable -Name Path "C:\Users\James\projects\fun\bin"
-new-item -path alias:less -Value "C:\Program Files (x86)\PowerShell Community Extensions\Pscx3\Pscx\Apps\less.exe"
 
+
+
+$candidatePaths = "C:\Program Files (x86)\Git\bin",
+                  "C:\Users\James\sysinternals",
+                  "C:\Program Files (x86)\ErgoEmacs\bin",
+                  "C:\Users\James\projects\fun\bin",
+                  "C:\Users\James\Documents\fun\bin"
+                  
+foreach ($path in $candidatePaths) {
+    if (Test-Path $path) {
+        Add-PathVariable -Name Path $path
+    }
+}
+
+# make this alias to make sure that we use the Pscx version of less if available
+$less = "C:\Program Files (x86)\PowerShell Community Extensions\Pscx3\Pscx\Apps\less.exe" 
+if (Test-Path $less) {
+    New-Item -Path alias:less -Value $less | Out-Null
+}
 
 # set this so emacs knows where to look for the .emacs file
 $env:HOME="C:\Users\James"
