521a37ca76b410e6a1e9b6a44f5721891b2426fd
jamesbharvey <pi@raspberrypi>
2021-08-07 22:12:13 +0900

    added things so i don't have to redo them on failure

---
+++ bin/start_google_assistant.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source /home/pi/env/bin/activate
+
+google-assistant-demo \
+    --project-id pi-my-life--assistant-acc3e \
+    --device-model-id pi-my-life--assistant-acc3e-pi-google-assistant-dpuhov
