f5dec6205d08d82826d7350c9c8fefa0dca79f82
jamesbharvey <james.harvey@yahoo.com>
2021-11-24 15:04:26 +0900

    * added command for how to get auth code

--- bin/start_google_assistant.sh
+++ bin/start_google_assistant.sh
@@ -5,3 +5,15 @@ source /home/pi/env/bin/activate
 google-assistant-demo \
     --project-id pi-my-life--assistant-acc3e \
     --device-model-id pi-my-life--assistant-acc3e-pi-google-assistant-dpuhov
+
+
+exit
+
+##
+## re-run this if there is an auth error.
+##
+
+google-oauthlib-tool --client-secrets ~/googleassistant/credentials.json \
+		     --scope https://www.googleapis.com/auth/assistant-sdk-prototype \
+		     --scope https://www.googleapis.com/auth/gcm \
+		     --save --headless
