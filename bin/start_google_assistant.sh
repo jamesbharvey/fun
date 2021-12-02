6f4bae0922c86e38663622e33dd6fa6e52aec29b
jamesbharvey <james.harvey@yahoo.com>
2021-12-02 18:11:29 +0900

    tweaked cbthumb added comments to start_google_assistant.sh

--- bin/start_google_assistant.sh
+++ bin/start_google_assistant.sh
@@ -19,4 +19,11 @@ google-oauthlib-tool --client-secrets ~/googleassistant/credentials.json \
 		     --save --headless
 
 
-#https://pimylifeup.com/raspberry-pi-google-assistant/
+https://pimylifeup.com/raspberry-pi-google-assistant/
+
+##
+## record and play to test mike and speaker
+##
+
+arecord --format=S16_LE --duration=5 --rate=16000 --file-type=raw out.raw
+aplay --format=S16_LE --rate=16000 out.raw
