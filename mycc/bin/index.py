2c5abe1820421802cd964abe86dc0bccc0deb9e2
Harvey <james.harvey@p41807.intra.rakuten.co.jp>
2021-12-08 16:26:22 +0900

    refactored index.py a bit added fields.txt

--- mycc/bin/index.py
+++ mycc/bin/index.py
@@ -1,56 +1,74 @@
 #!/usr/bin/env python
+import os
 import warnings
 import zipfile
 import glob
 import subprocess
+import xml.etree.ElementTree as ET
 
-def getXmlFilesFromZipFile(zfile):
-    xmlfilenames = list(filter(lambda x: x.lower().endswith('.xml'), zfile.namelist()))
-    for xmlfilename in xmlfilenames:
-        xmlpath = zfile.extract(xmlfilename)
-        xmlfile = open(xmlpath)
-        #print(xmlfile.readlines())
-        xmlfile.close()
-    return xmlfilenames
+def xmlTreeToDictionary(dict,xmlpath):
+    xmlpath = os.path.basename(xmlpath)
+    tree = ET.parse(xmlpath)
+    root = tree.getroot()
+    if root.tag != 'ComicInfo':
+        warnings.warn("tried to parse xml file[" + xmlpath + "] that is not ComicInfo")
+    for child in root:
+        dict[child.tag] = child.text
+    return dict
 
-def getXmlFilesFromRarFile(rfilename):
-    completedProcess = subprocess.run(["unrar", "lb", rfilename], capture_output=True)
+def setNonXmlFieldsForDictToIndex(dictToIndex,filepath):
+    dictToIndex['FileName'] = os.path.basename(filepath)
+    dictToIndex['OsFilePath'] = filepath
+    return dictToIndex
+
+def getDictToIndexFromZipFile(zipfilename):
+    dictToIndex = {}
+    with zipfile.ZipFile(filename, "r") as zfile:
+        xmlfilenames = list(filter(lambda x: x.lower().endswith('.xml'), zfile.namelist()))
+        if len(xmlfilenames) != 1 and len(xmlfilenames) != 0:
+            warnings.warn("multiple xml files in .cbr[" + zipfilename + "]....")
+        for xmlfilename in xmlfilenames:
+            xmlpath = zfile.extract(xmlfilename)
+            dictToIndex = xmlTreeToDictionary(dictToIndex, xmlpath)
+        return setNonXmlFieldsForDictToIndex(dictToIndex, zipfilename)
+
+def getDictToIndexFromRarFile(rarfilename):
+    dict = {}
+    setNonXmlFieldsForDictToIndex(dict, rarfilename)
+    completedProcess = subprocess.run(["unrar", "lb", rarfilename], capture_output=True)
     if completedProcess.returncode != 0:
-        return []
+        warnings.warn("couldn't list contents of rarfile[" + rarfilename + "]")
+        return dict
     strings = []
     for bytes in completedProcess.stdout.splitlines():
         strings.append(str(bytes).rstrip('/n').lstrip("b'").rstrip("'"))
     xmlfilenames = list(filter(lambda x: x.lower().endswith('.xml'), strings))
+    if len(xmlfilenames) != 1 and len(xmlfilenames) != 0:
+        warnings.warn("multiple xml files in .cbr[" + rarfilename + "]....")
+        for filename in xmlfilenames:
+            warnings.warn("xml file name is [" + filename + "]", stacklevel=1)
     for xmlfilename in xmlfilenames:
-        print (rfilename + " : " + xmlfilename)
         completedProcess = subprocess.run(["unrar",
-                                          "e",
-                                          "-o+",
-                                          "-y",
-                                          "-inul",
-                                          rfilename,
-                                          xmlfilename])
+                                           "e",
+                                           "-o+",
+                                           "-y",
+                                           "-inul",
+                                           rarfilename,
+                                           xmlfilename])
         if completedProcess.returncode != 0:
-            warnings.warn("couldn't extract file[" + xmlfile + "] from rar archive[" + rfilename + "]")
+            warnings.warn("couldn't extract file[" + xmlfilename + "] from rar archive[" + rarfilename + "]")
             return []
-        xmlfile = open(xmlfilename)
-        print(xmlfile.readlines())
-        xmlfile.close()
-    return xmlfilenames
+        dict = xmlTreeToDictionary(dict,xmlfilename)
+    return dict
 
+os.chdir('/Users/james.harvey/Desktop/2021.04.21 Weekly Pack')
 
-for filename in glob.glob('*.[Cc][Bb][Zz]'):
-    zfile = zipfile.ZipFile(filename, "r")
-    xmlfilenames = getXmlFilesFromZipFile(zfile)
+for filename in glob.glob('*/*.[Cc][Bb][Zz]'):
+    print(filename)
+    dictToIndex = getDictToIndexFromZipFile(filename)
+    print(dictToIndex)
 
-for filename in glob.glob('*.[Cc][Bb][Rr]'):
-    xmlfilenames = getXmlFilesFromRarFile(filename)
-    print(xmlfilenames)
-
-
-
-exit()
-
-# list file information
-# for info in file.infolist():
-#    print info.filename, info.date_time, info.file_size
+for filename in glob.glob('*/*.[Cc][Bb][Rr]'):
+    print(filename)
+    dictToIndex = getDictToIndexFromRarFile(filename)
+    print(dictToIndex)
