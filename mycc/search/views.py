d779c0ca23ffb783f1b82272b030271c9830d7b5
james.harvey <james.harvey@yahoo.com>
2021-12-09 17:46:07 +0900

    django can now display comics from mongo

--- mycc/search/views.py
+++ mycc/search/views.py
@@ -1,10 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import loader
+import pymongo
+import urllib.parse
+
+mongoClient = pymongo.MongoClient()
+mongoDbName = mongoClient['mycc']
+mongoCollection = mongoDbName['comics']
+
 
 def index(request):
     template = loader.get_template('search/index.html')
+    cursor = mongoCollection.find()
+    comicList = []
+    for comic in cursor:
+        rootRelativePath = str(comic['AbsoluteFilePath']).replace("/Users/james.harvey/Desktop/", "")
+        comic['url'] = "http://127.0.0.1:8080/" + urllib.parse.quote(rootRelativePath)
+        comicList.append(comic)
+    numComics = len(comicList)
     context = {
-        'latest_question_list': '',
+        'comics': comicList,
+        'numComics': numComics
     }
     return HttpResponse(template.render(context, request))
 
