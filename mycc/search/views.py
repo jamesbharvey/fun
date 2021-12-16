b4ebd76ae42e203edda872872b05f79ee4eb6ada
james.harvey <james.harvey@yahoo.com>
2021-12-16 10:36:44 +0900

    Put ComicInfo.xml info into tooltip

--- mycc/search/views.py
+++ mycc/search/views.py
@@ -21,13 +21,27 @@ def index(request):
         cursor = mongo_collection.find(query)
         absolute_path_roots = [["/Users/james.harvey/Desktop/", "http://127.0.0.1:8080/"]]
         for comic in cursor:
+            fields_for_link_title = []
+            for field in (
+                          'Series',
+                          'Number',
+                          'Title',
+                          'Writer',
+                          'Penciller',
+                          'Genre',
+                          'Publisher',
+                          'Format',
+                          'Summary',
+                          'FileName'):
+                if field in comic:
+                    fields_for_link_title.append(field + " : " + str(comic[field]))
+            comic['LinkTitle'] = "\n".join(fields_for_link_title)
             absolute_file_path = str(comic['AbsoluteFilePath'])
             for path_url_tuple in absolute_path_roots:
                 root_relative_path = absolute_file_path.lstrip(path_url_tuple[0])
-            comic['url'] = path_url_tuple[1] + urllib.parse.quote(root_relative_path)
+            comic['Url'] = path_url_tuple[1] + urllib.parse.quote(root_relative_path)
             comic_list.append(comic)
     num_comics = len(comic_list)
-
     context['comics'] = comic_list
     context['num_comics'] = num_comics
     return HttpResponse(template.render(context, request))
