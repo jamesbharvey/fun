71f3a7b37f3c77213e0786d1f2a957126c90b1da
james.harvey <james.harvey@yahoo.com>
2021-12-20 12:36:35 +0900

    er, working (mostly but not correctly) so commiting in case i need to rollback to this mostly-correct state

--- mycc/search/static/search/advanced_search.js
+++ mycc/search/static/search/advanced_search.js
@@ -1,4 +1,3 @@
-'use strict';
 
 const e = React.createElement;
 
@@ -6,23 +5,39 @@ class AdvancedButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = { advanced: false };
+    this.toggleAdvancedSearchVisible()
   }
 
   render() {
     if (this.state.advanced) {
       return e('button',
-          { onClick: () => this.setState({advanced: false})},
-          'Simple'
+          { onClick: () => { this.setState({advanced: false}); this.toggleAdvancedSearchVisible();}
+          },
+          'Advanced'
       );
     }
 
     return e(
       'button',
-      { onClick: () => this.setState({ advanced: true }) },
-      'Advanced'
+      { onClick: () => { this.setState({ advanced: true });
+                                  this.toggleAdvancedSearchVisible();} },
+      'Simple'
     );
+  }
+  //
+  // this is hacky(using jquery inside react) and against the spirit of React, but I'm not redoing it right
+  // Probably I should rewrite the whole search page in react and get rid of the python templates
+  //
+
+  toggleAdvancedSearchVisible() {
+    if (this.state.advanced) {
+      $('#advanced_search').show()
+    } else {
+      $('#advanced_search').hide()
+    }
   }
 }
 
-const domContainer = document.querySelector('#advanced_search');
-ReactDOM.render(e(AdvancedButton), domContainer);
+const buttonDomContainer = document.querySelector('#advanced_search_button');
+ReactDOM.render(e(AdvancedButton), buttonDomContainer);
+
