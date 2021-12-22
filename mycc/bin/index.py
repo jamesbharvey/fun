7e480d2de88a8d794bb11fae423a073b99ff6193
james.harvey <james.harvey@yahoo.com>
2021-12-22 17:30:08 +0900

    use pypdf4 to extract info from pdfs

--- mycc/bin/index.py
+++ mycc/bin/index.py
@@ -7,6 +7,7 @@ import glob
 import subprocess
 import xml.etree.ElementTree
 import pymongo
+from PyPDF4 import PdfFileReader
 from pathlib import Path
 
 
@@ -60,6 +61,8 @@ class ComicFileHandler:
             return
         # yes this is crude and will not work for Euro Comics, etc.
         # picked 82 for 80 page giants + cover + "scanned by" page
+        #if 'PageCount' in self.to_index:
+        #    page_count = self.to_index['PageCount']
         if page_count > 82:
             self.to_index['Format'] = 'Trade'
         else:
@@ -97,8 +100,7 @@ class ComicFileHandler:
                 warnings.warn("xml file name is [" + filename + "]", stacklevel=1)
         # there are several rar libraries in python but none
         # of them are reliable enough or support enough features to unrar
-        # files from the wild reliably,
-        # so we use the official free-as-in-beer version
+        # files from the wild reliably, so we use the official free-as-in-beer version
         # which must be installed on your path
         for xml_file_name in xml_file_names:
             completed_process = subprocess.run(["unrar",
@@ -115,10 +117,16 @@ class ComicFileHandler:
             self.parse_and_set_xml_fields(xml_file_name)
         self.set_format(page_count)
 
-
     def parse_pdf_file(self):
         self.set_non_xml_fields()
-
+        with open(self.archive_path, 'rb') as f:
+            pdf = PdfFileReader(f)
+            doc_info = pdf.getDocumentInfo()
+            number_of_pages = pdf.getNumPages()
+            self.to_index['Writer'] = doc_info.author
+            self.to_index['Title'] = doc_info.title
+            self.to_index['PageCount'] = number_of_pages
+            self.set_format(number_of_pages)
 
 mongoClient = pymongo.MongoClient()
 mongoDbName = mongoClient['mycc']
