4d1a495d2cc9e3d62ad32223e9d41e13471b205d
James Harvey <james.harvey@yahoo.com>
2022-02-21 16:02:02 +0900

    * added field weights to text index

--- mycc/bin/index.py
+++ mycc/bin/index.py
@@ -227,7 +227,18 @@ mongoCollection.create_index([("FileName", "text"),
                               ("Writer", "text"),
                               ("Year", "text"),
                               ("Number", "text"),
-                              ("AbsoluteFilePath", "text")])
+                              ("AbsoluteFilePath", "text")],
+                             weights={
+                                 'FileName': 10,
+                                 'Series': 5,
+                                 "Title":1,
+                                 "Number": 5,
+                                 'Summary': 1,
+                                 "Writer": 1,
+                                 "Year": 1,
+                                 "AbsoluteFilePath": 5,
+                             }
+                             )
 
 for directory in directories:
     if os.path.exists(directory):
