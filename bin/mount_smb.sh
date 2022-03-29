5c96d33427b136e9cbb5d7f975f578830c0f0211
jamesbharvey <james.harvey@yahoo.com>
2022-03-29 10:35:10 +0900

    * not sure but don't want to lose...

--- bin/mount_smb.sh
+++ bin/mount_smb.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo mount -t cifs -o rw,vers=2.0,credentials=/home/pi/.shield_credentials  //192.168.11.7/internal /mnt/shield
+#sudo mount -t cifs -o rw,vers=2.0,credentials=/home/pi/.shield_credentials  //192.168.11.7/internal /mnt/shield
+sudo mount -t cifs -o rw,vers=2.0,credentials=/home/pi/.stick_credentials  //192.168.11.23/SEAGATE8TB /mnt/stick8tb
--- grovepi/Home_Weather_Display.py
+++ grovepi/Home_Weather_Display.py
@@ -17,46 +17,72 @@ from grovepi import *
 from grove_rgb_lcd import *
 from time import sleep
 from math import isnan
+from time import localtime, strftime
+import socket
 
 dht_sensor_port = 2 # connect the DHt sensor to port 7
 dht_sensor_type = 0 # use 0 for the blue-colored sensor and 1 for the white-colored sensor
+last_sent_time = 0
+UDP_PORT = 2004
+UDP_IP = "192.168.11.23"
 
 # set green as backlight color
 # we need to do it just once
 # setting the backlight color once reduces the amount of data transfer over the I2C line
 setRGB(0,255,0)
+setText("")
 
+def send_udp(message):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+    sock.sendto(bytes(message, "utf-8"), (UDP_IP, UDP_PORT))
+
+def send_to_graphite(temp,humi):
+    temp_message = "mysensors.study.temperature " + temp + " -1"
+    humi_message = "mysensors.study.humidity " + humi + " -1"
+    send_udp(temp_message)
+    send_udp(humi_message)
+    
+    
 while True:
-	try:
+    try:
         # get the temperature and Humidity from the DHT sensor
-		[ temp,hum ] = dht(dht_sensor_port,dht_sensor_type)
+        [ temp,hum ] = dht(dht_sensor_port,dht_sensor_type)
+        print(strftime("%Y-%b-%d %H:%M:%S",localtime()))
+        print( temp, "C ", hum,"% Hum.\t")
 
-		print("temp =", temp, "C\thumidity =", hum,"%")
 
-		# check if we have nans
-		# if so, then raise a type error exception
-		if isnan(temp) is True or isnan(hum) is True:
-			raise TypeError('nan error')
+        # check if we have nans
+        # if so, then raise a type error exception
+        if isnan(temp) is True or isnan(hum) is True:
+            raise TypeError('nan error')
 
-		t = str(temp)
-		h = str(hum)
+        t = str(temp)
+        h = str(hum)
 
         # instead of inserting a bunch of whitespace, we can just insert a \n
         # we're ensuring that if we get some strange strings on one line, the 2nd one won't be affected
-		setText_norefresh("Temp:" + t + "C\n" + "Humidity :" + h + "%")
+        setText_norefresh(strftime("%b %d %H:%M",localtime())
+        + "\n" + t + "C " +  h + "% Hum")
 
-	except (IOError, TypeError) as e:
-		print(str(e))
-		# and since we got a type error
-		# then reset the LCD's text
-		setText("")
+        if (time.time() - last_sent_time) > 30:
+            print("send packet")
+            last_sent_time = time.time()
+            send_to_graphite(t,h)
+        else:
+            print("don't send packet")
 
-	except KeyboardInterrupt as e:
-		print(str(e))
-		# since we're exiting the program
-		# it's better to leave the LCD with a blank text
-		setText("")
-		break
+    except (IOError, TypeError) as e:
+        print(str(e))
+        # and since we got a type error
+        # then reset the LCD's text
+        setText("")
 
-	# wait some time before re-updating the LCD
-	sleep(1)
+    except KeyboardInterrupt as e:
+        print(str(e))
+        # since we're exiting the program
+        # it's better to leave the LCD with a blank text
+        setText("")
+        break
+
+    # wait some time before re-updating the LCD
+    sleep(5)
