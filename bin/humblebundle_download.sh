e9553552b644a597502f7d445b71f677a91226b7
James Harvey <james.harvey@yahoo.com>
2022-04-01 15:22:19 +0900

    updated for new host

--- bin/humblebundle_download.sh
+++ bin/humblebundle_download.sh
@@ -1,6 +1,6 @@
 
-source ~/humbledownload.env/bin/activate
+cd ~/humblebundle-downloader
+pipenv shell
+cd /mnt/seagate8tb
 
-cd /mnt/stick8tb
-hbd --cookie-file cookies.txt --update --library-path humblebundle.all --progress -p ebook
-# hbd --cookie-file /mnt/stick8tb/humblebundle.com_cookies.txt --library-path humblebundle.all --keys XXXXX --progress -p ebook
+hbd --cookie-file humblebundle.com_cookies.txt --library-path humblebundle.all --keys XXXXX --progress -p ebook
