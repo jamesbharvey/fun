e82ee1055d1b6d46fe5060c6fdc728dc18b0b9a8
James Harvey <james.harvey@yahoo.com>
2022-08-04 18:15:39 +0900

    added note on how to get cookies.txt correctly to humblebundle_download.sh

--- bin/humblebundle_download.sh
+++ bin/humblebundle_download.sh
@@ -1,3 +1,11 @@
+##
+## if you get a KeyError['product']
+## 1) use chrome to log in to Humble Bundle
+## 2) navigate to the humble bundle page
+## 3) use the getcookies.txt extenstion to dowload the humblebundle.com coookies
+## 4) copy the humblebundle.com_cookies.txt file to /mnt/seagatetb
+## 5) try again
+##
 
 cd ~/humblebundle-downloader
 pipenv shell
