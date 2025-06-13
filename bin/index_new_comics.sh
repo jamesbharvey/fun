f6b6242ebce61b1e8b96074da0d2396438c8c596
James Harvey <james.harvey@yahoo.com>
2025-06-13 22:12:29 +0900

    path tweak

--- bin/index_new_comics.sh
+++ bin/index_new_comics.sh
@@ -7,5 +7,11 @@ rm -f /mnt/buffalo8tb/torrents.done/mycc.indexed
 rm -f /mnt/buffalo8tb/torrents.done/index.html
 cd ~/fun/mycc
 
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+
+
 
 pipenv run bin/index.py --production
